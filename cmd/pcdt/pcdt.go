b7cdf0fd9a59ec3253463da54c8c070fe220b497
xerosic <42121005+xerosic@users.noreply.github.com>
2025-01-12 23:47:17 +0100

    added broken decompression FUNCTIONALITIES (sorry 4 typo)

--- cmd/pcdt/pcdt.go
+++ cmd/pcdt/pcdt.go
@@ -10,28 +10,52 @@ import (
 
 func main() {
 	// Define command line flags
+	extract := flag.Bool("x", false, "Decompress a .pakd file")
 	inputDir := flag.String("i", "", "Input directory to compress")
+	inputFile := flag.String("f", "", "Input .pakd file to decompress")
+	outputDir := flag.String("d", "", "Output directory for decompression")
 	outputFile := flag.String("o", "output.pakd", "Output .pakd file")
 	encrypt := flag.Bool("e", false, "Enable encryption")
 
 	// Parse command line flags
 	flag.Parse()
 
-	// Check if input directory is provided
-	if *inputDir == "" {
-		fmt.Println("Error: Input directory is required")
-		fmt.Println("Usage: pcdt -i <input_dir> [-o output.pakd] [-e]")
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
+	// Check the mode
+	if *extract {
+		// Decompression mode
+		if *inputFile == "" || *outputDir == "" {
+			fmt.Println("Error: Input file and output directory are required for decompression")
+			fmt.Println("Usage: pcdt -mode decompress -f <input.pakd> -d <output_dir>")
+			flag.PrintDefaults()
+			os.Exit(1)
+		}
 
-	// Check if input directory exists
-	if _, err := os.Stat(*inputDir); os.IsNotExist(err) {
-		fmt.Printf("Error: Directory '%s' does not exist\n", *inputDir)
-		os.Exit(1)
-	}
+		// Check if input file exists
+		if _, err := os.Stat(*inputFile); os.IsNotExist(err) {
+			fmt.Printf("Error: File '%s' does not exist\n", *inputFile)
+			os.Exit(1)
+		}
 
-	// Create the packd file
-	packd.CreatePackdFileFromDirectory(*inputDir, *outputFile, *encrypt)
-	fmt.Printf("Successfully created packd archive: %s\n", *outputFile)
+		// Extract the packd file
+		packd.ExtractPackdFileToDirectory(*inputFile, *outputDir)
+		fmt.Printf("Successfully extracted archive to: %s\n", *outputDir)
+	} else {
+		// Compression mode
+		if *inputDir == "" {
+			fmt.Println("Error: Input directory is required for compression")
+			fmt.Println("Usage: pcdt -mode compress -i <input_dir> [-o output.pakd] [-e]")
+			flag.PrintDefaults()
+			os.Exit(1)
+		}
+
+		// Check if input directory exists
+		if _, err := os.Stat(*inputDir); os.IsNotExist(err) {
+			fmt.Printf("Error: Directory '%s' does not exist\n", *inputDir)
+			os.Exit(1)
+		}
+
+		// Create the packd file
+		packd.CreatePackdFileFromDirectory(*inputDir, *outputFile, *encrypt)
+		fmt.Printf("Successfully created packd archive: %s\n", *outputFile)
+	}
 }
--- packd.go
+++ packd.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/klauspost/compress/zstd"
 )
@@ -14,28 +15,30 @@ import (
 type PIE [8]byte
 
 type FirstHeader struct {
-	isEncrypted   [1]byte
-	miscellaneous [7]byte
+	IsEncrypted   [1]byte
+	Miscellaneous [7]byte
 }
 
 type Header struct {
-	pathLength uint16
-	dataLength uint64
+	PathLength uint16
+	DataLength uint64
 }
 
 type Box struct {
-	header Header
-	path   []byte
-	data   []byte
+	BoxHeader Header
+	Path      []byte
+	Data      []byte
 }
 
 type PackdFile struct {
 	PIE         PIE
-	firstHeader FirstHeader
-	boxes       []Box
+	FirstHeader FirstHeader
+	Boxes       []Box
 }
 
 func CreatePackdFileFromDirectory(path string, outputFileName string, isEncrypted bool) {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	// Create a new PackdFile
 	pf := PackdFile{}
 
@@ -46,9 +49,9 @@ func CreatePackdFileFromDirectory(path string, outputFileName string, isEncrypte
 
 	// Set the first header
 	if isEncrypted {
-		pf.firstHeader.isEncrypted = [1]byte{0x01}
+		pf.FirstHeader.IsEncrypted = [1]byte{0x01}
 	} else {
-		pf.firstHeader.isEncrypted = [1]byte{0x00}
+		pf.FirstHeader.IsEncrypted = [1]byte{0x00}
 	}
 
 	// Create zstd encoder
@@ -82,16 +85,16 @@ func CreatePackdFileFromDirectory(path string, outputFileName string, isEncrypte
 		header := Header{}
 
 		// Set the box header path length
-		header.pathLength = uint16(len(file.Name()))
+		header.PathLength = uint16(len(file.Name()))
 
 		// Create a new box
 		box := Box{}
 
 		// Set the box header
-		box.header = header
+		box.BoxHeader = header
 
 		// Set the box path
-		box.path = []byte(file.Name())
+		box.Path = []byte(file.Name())
 
 		// Compress the data
 		compressedData := encoder.EncodeAll(data, nil)
@@ -101,10 +104,10 @@ func CreatePackdFileFromDirectory(path string, outputFileName string, isEncrypte
 		}
 
 		// Set the box header data length
-		header.dataLength = uint64(len(data))
+		header.DataLength = uint64(len(data))
 
 		// Set the box data
-		box.data = compressedData
+		box.Data = compressedData
 
 		// Append the box to the boxes slice
 		boxes = append(boxes, box)
@@ -112,7 +115,7 @@ func CreatePackdFileFromDirectory(path string, outputFileName string, isEncrypte
 	}
 
 	// Set the PackdFile boxes
-	pf.boxes = boxes
+	pf.Boxes = boxes
 
 	// Create the output file
 	outFile, err := os.Create(outputFileName)
@@ -127,27 +130,103 @@ func CreatePackdFileFromDirectory(path string, outputFileName string, isEncrypte
 	}
 
 	// Write FirstHeader
-	if err := binary.Write(outFile, binary.LittleEndian, pf.firstHeader); err != nil {
+	if err := binary.Write(outFile, binary.LittleEndian, pf.FirstHeader); err != nil {
 		log.Fatal(err)
 	}
 
 	// Write each box
-	for _, box := range pf.boxes {
+	for _, box := range pf.Boxes {
 		// Write header
-		if err := binary.Write(outFile, binary.LittleEndian, box.header); err != nil {
+		if err := binary.Write(outFile, binary.LittleEndian, box.BoxHeader); err != nil {
 			log.Fatal(err)
 		}
 
 		// Write path
-		if _, err := outFile.Write(box.path); err != nil {
+		if _, err := outFile.Write(box.Path); err != nil {
 			log.Fatal(err)
 		}
 
 		// Write compressed data
-		if _, err := outFile.Write(box.data); err != nil {
+		if _, err := outFile.Write(box.Data); err != nil {
 			log.Fatal(err)
 		}
 	}
 
+	return
+}
+
+func ExtractPackdFileToDirectory(inputFileName string, outputPath string) {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	// Open input file
+	inFile, err := os.Open(inputFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer inFile.Close()
+
+	// Create decoder
+	decoder, err := zstd.NewReader(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer decoder.Close()
+
+	// Read and verify PIE
+	pie := PIE{}
+	if err := binary.Read(inFile, binary.LittleEndian, &pie); err != nil {
+		log.Fatal(err)
+	}
+	expectedPIE := PIE{0x3C, 0x21, 0x50, 0x41, 0x4B, 0x44, 0x21, 0x3E}
+	if pie != expectedPIE {
+		log.Fatal("error: invalid file format (no PIE present / invalid PIE)")
+	}
+
+	// Read FirstHeader
+	firstHeader := FirstHeader{}
+	if err := binary.Read(inFile, binary.LittleEndian, &firstHeader); err != nil {
+		log.Fatal(err)
+	}
+
+	// Create output directory if it doesn't exist
+	if err := os.MkdirAll(outputPath, 0755); err != nil {
+		log.Fatal(err)
+	}
+
+	// Read and extract boxes
+	for {
+		// Read header
+		header := Header{}
+		if err := binary.Read(inFile, binary.LittleEndian, &header); err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatal(err)
+		}
+		// Read path
+		path := make([]byte, header.PathLength)
+		if _, err := io.ReadFull(inFile, path); err != nil {
+			log.Fatal(err)
+		}
+
+		// Read compressed data
+		compressedData := make([]byte, header.DataLength)
+		if _, err := io.ReadFull(inFile, compressedData); err != nil {
+			log.Fatal(err)
+		}
+
+		// Decompress data
+		decompressedData, err := decoder.DecodeAll(compressedData, nil)
+		if err != nil {
+			log.Fatal(err)
+
+			// Write to file
+			outFilePath := filepath.Join(outputPath, string(path))
+			if err := os.WriteFile(outFilePath, decompressedData, 0644); err != nil {
+				log.Fatal(err)
+			}
+		}
+
+	}
 	return
 }
